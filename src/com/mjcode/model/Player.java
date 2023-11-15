1e67ae4b2260546ef41b804597ab8ed2019c99dc
Mallay Jalloh <mallayjalloh@icloud.com>
2023-11-15 17:53:52 -0500

    Revert "Implemented the addPiece, getPieces, removePiece, and king methods."
    This reverts commit 22627ecda7cfb03b1fa271695ed3ae4df41e44cf.

--- src/com/mjcode/model/Player.java
+++ src/com/mjcode/model/Player.java
@@ -1,17 +1,14 @@
 package com.mjcode.model;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public abstract class Player {
 
     // Declare fields
     private Game game;
-    private Piece[] pieces;
+    private Piece[] piece;
     private Color color;
     private int direction;
-    private ArrayList<Piece> playerList;
 
     /**
      * No-args constructor initializes the fields appropriately
@@ -20,55 +17,47 @@ public abstract class Player {
         this.game = null;
         this.color = null;
         this.direction = 0;
-        this.playerList = new ArrayList<>();
     }
 
     /**
      * The addPiece creates a new piece and adds it to the player's list
      * of pieces
-     * @param initialPosition initial position
+     * @param initialPosition Starting position
      * @return The created piece
      */
     public Piece addPiece(Tile initialPosition) {
-        // Implement - LIST
-        if (initialPosition.getOccupant() != null) {
-            Piece newPiece = new Piece(initialPosition.getOccupant().getPlayer(), initialPosition, direction); // Create a new piece
-            playerList.add(newPiece); // Add the piece to the player's list
-            return newPiece;
-        } else {
-            return null;
-        }
+        return null;
     }
 
     /**
      * The executeMove method reflects the move specified
      * @param move The move to be executed
      */
-    public abstract void executeMove(Move move);
+    public void executeMove(Move move) {
+
+    }
 
     /**
      * Starts the game of checkers
      */
-    public abstract Game getGame();
+    public Game getGame() {
+        return this.game;
+    }
 
     /**
      * The setGame method sets the game of checkers
      * @param game The game field
      */
     public void setGame(Game game) {
-        this.game = game;
+
     }
 
     /**
-     * The getPiece method returns all the player's remaining pieces
+     * The getPiece method gets all the player's remaining pieces
      * @return An array of pieces
      */
-    public Piece[] getPieces() {
-        // Implement - ARRAY
-        // Convert arrayList to an array
-        pieces = new Piece[playerList.size()];
-        pieces = playerList.toArray(pieces);
-        return pieces;
+    public Piece[] getPiece() {
+        return this.piece;
     }
 
     /**
@@ -76,17 +65,17 @@ public abstract class Player {
      * @return The number of pieces this player has remaining
      */
     public int getNumPieces() {
-        // Implement - INTEGER
         return 0;
     }
 
     /**
      * The removePiece method removes the provided piece from the player's list of pieces
      * @param remove The provided piece from the player's list
+     *
+     * ?? Not sure if it returns something or if it should be a void method ??
      */
     public void removePiece(Piece remove) {
-        // Implement - LIST
-        playerList.remove(remove);
+
     }
 
     /**
@@ -117,7 +106,7 @@ public abstract class Player {
      * @param piece The piece to be set as king
      */
     public void king(Piece piece) {
-        piece.king();
+
     }
 
 }
